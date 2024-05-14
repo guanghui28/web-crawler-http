5b30202f8f552578fd15e4609da5251d14ff2ac0
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-05-14 19:19:29 +0700

    normalize url and test

--- crawl.test.js
+++ crawl.test.js
@@ -1,9 +1,30 @@
 import { test, expect } from "@jest/globals";
 import { normalizeURL } from "./crawl.js";
 
-test("normalizeURL", () => {
-	const input = "";
+test("normalizeURL strip protocol", () => {
+	const input = "https://blog.boot.dev/path";
 	const actual = normalizeURL(input);
-	const expected = "";
+	const expected = "blog.boot.dev/path";
+	expect(actual).toEqual(expected);
+});
+
+test("normalizeURL strip trailing slash", () => {
+	const input = "https://blog.boot.dev/path/";
+	const actual = normalizeURL(input);
+	const expected = "blog.boot.dev/path";
+	expect(actual).toEqual(expected);
+});
+
+test("normalizeURL capitals", () => {
+	const input = "https://BLOG.boot.dev/path";
+	const actual = normalizeURL(input);
+	const expected = "blog.boot.dev/path";
+	expect(actual).toEqual(expected);
+});
+
+test("normalizeURL strip http", () => {
+	const input = "https://blog.boot.dev/path";
+	const actual = normalizeURL(input);
+	const expected = "blog.boot.dev/path";
 	expect(actual).toEqual(expected);
 });
