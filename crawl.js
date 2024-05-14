d40e425bd857b85b31fb4b93363d2f349ad5557c
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-05-14 20:21:02 +0700

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -35,3 +35,30 @@ export function getURLsFromHTML(htmlBody, baseURL) {
 	}
 	return urls;
 }
+
+export async function crawlPage(currentURL) {
+	console.log(`actively crawling: ${currentURL}`);
+
+	try {
+		const resp = await fetch(currentURL);
+
+		if (resp.status > 399) {
+			console.log(
+				`Error in fetch with status code: ${resp.status} on page: ${currentURL}`
+			);
+			return;
+		}
+
+		const contentType = resp.headers.get("content-type");
+		if (!contentType.includes("text/html")) {
+			console.log(
+				`Non html response, content type: ${contentType}, on page: ${currentURL}`
+			);
+			return;
+		}
+
+		console.log(await resp.text());
+	} catch (error) {
+		console.log(`error in fetch: ${error.message} on page: ${currentURL}`);
+	}
+}
